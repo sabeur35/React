fc880404ec9951cb7eeadbc5b693f8ff319108f5
BESSAOUD SABEUR <sabeur35@gmail.com>
2024-03-30 14:41:27 +0100

    t

---
+++ proj1/myproject/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Products from './COMPONENTS/Products';
+
+
+function App() {
+  return (
+    <div >
+      
+      <div className="container">
+      <div className="row text-center">
+      
+      <div className="col"><Products></Products></div>
+      
+
+      
+
+       </div>
+       </div>
+  </div>
+  );
+}
+
+
+
+export default App;
